414319c5c8440593caf9fac6f324f9bf39765126
Paul Morales <pomorales@gmail.com>
2012-09-29 13:52:16 -0700

    Stuff Olmec Chan Says
    Added logic to determine what Olmec says. Changed some constant names.

--- constants.lua
+++ constants.lua
@@ -13,19 +13,30 @@ Constants.CENTER = Constants.SCREEN / 2
 -- Debug mode.
 Constants.DEBUG_MODE = true
 
+-- Olmec Chan's speech thing
+Constants.OLMEC_SPEECH_TIME = 1000
+
+-- Olmec Chan's topics of interest
+Constants.OLMECSUBJECT_INTRO = "INTRO"
+Constants.OLMECSUBJECT_WORLD = "WORLD"
+Constants.OLMECSUBJECT_TEMPLE = "TEMPLE"
+Constants.OLMECSUBJECT_ROVER = "ROVER"
+Constants.OLMECSUBJECT_FIGHT = "FIGHT"
+Constants.OLMECSUBJECT_DEFEAT = "DEFEAT"
+
 -- Olmec Chan's lines.
-Constants.OLMECTALK.INTRO = "Earth sends another metal man to this planet. Welcome to die!"
-Constants.OLMECTALK.WORLD1 = "Curiously, there are no cats on Mars."
-Constants.OLMECTALK.WORLD2 = "You should try anti-gravity. I'd let you borrow the book, but it's hard to put down."
-Constants.OLMECTALK.WORLD3 = "There's a giant pit over there. Wouldn't you be curious to see what's at the bottom?"
-Constants.OLMECTALK.WORLD4 = "What do you call a lost Mars rover? Rusty."
-Constants.OLMECTALK.TEMPLE1 = "I locked the other ones away. You could say they've had a hard time."
-Constants.OLMECTALK.TEMPLE2 = "You should step aside. This temple has stairs!"
-Constants.OLMECTALK.TEMPLE3 = "You should've talked to those Vikings. Too bad you don't know Norse Code."
-Constants.OLMECTALK.ROVER1 = "Your antennae make a good couple. I'm sure it'd be a great reception."
-Constants.OLMECTALK.ROVER2 = "There was an opportunity to say something here, but I may have missed it."
-Constants.OLMECTALK.ROVER3 = "Watch out! It's been hacked!"
-Constants.OLMECTALK.FIGHT = "Stunned? Don't be. I'm stone cold."
-Constants.OLMECTALK.DEFEAT = "Defeated?! Inconceivable!!"
+Constants.OLMECTALK_INTRO = "Earth sends another metal man to this planet. Welcome to die!"
+Constants.OLMECTALK_WORLD1 = "Curiously, there are no cats on Mars."
+Constants.OLMECTALK_WORLD2 = "You should try anti-gravity. I'd let you borrow the book, but it's hard to put down."
+Constants.OLMECTALK_WORLD3 = "There's a giant pit over there. Wouldn't you be curious to see what's at the bottom?"
+Constants.OLMECTALK_WORLD4 = "What do you call a lost Mars rover? Rusty."
+Constants.OLMECTALK_TEMPLE1 = "I locked the other ones away. You could say they've had a hard time."
+Constants.OLMECTALK_TEMPLE2 = "You should step aside. This temple has stairs!"
+Constants.OLMECTALK_TEMPLE3 = "You should've talked to those Vikings. Too bad you don't know Norse Code."
+Constants.OLMECTALK_ROVER1 = "Your antennae make a good couple. I'm sure it'd be a great reception."
+Constants.OLMECTALK_ROVER2 = "There was an opportunity to say something here, but I may have missed it."
+Constants.OLMECTALK_ROVER3 = "Watch out! It's been hacked!"
+Constants.OLMECTALK_FIGHT = "Stunned? Don't be. I'm stone cold."
+Constants.OLMECTALK_DEFEAT = "Defeated?! Inconceivable!!"
 
 return Constants
--- states/play.lua
+++ states/play.lua
@@ -11,6 +11,14 @@ function PlayState:init()
     -- Load the map stuff
     self.ground = Ground()
 
+    -- Initialize all the crap Olmec Chan says
+    self.olmecSays = ""
+    self.olmecSpeakTime = Constants.OLMEC_SPEECH_TIME
+    
+    -- Initialize the subjects that have more than one line
+    self.olmecWorldLines = {Constants.OLMECTALK_WORLD1, Constants.OLMECTALK_WORLD2, Constants.OLMECTALK_WORLD3, Constants.OLMECTALK_WORLD4}
+    self.olmecTempleLines = {Constants.OLMECTALK_TEMPLE1, Constants.OLMECTALK_TEMPLE2, Constants.OLMECTALK_TEMPLE3}
+    self.olmecRoverLines = {Constants.OLMECTALK_ROVER1, Constants.OLMECTALK_ROVER2, Constants.OLMECTALK_ROVER3}
 end
 
 function PlayState:enter(previous)
@@ -41,6 +49,16 @@ function PlayState:update(dt)
             self.lastFpsTime = 0
         end
     end
+    
+    -- Update Olmec talk box
+    if self.olmecSpeakTime > 0 then
+        -- TODO: Olmec speaks!
+      
+        self.olmecSpeakTime = self.olmecSpeakTime - 1
+    else
+        self.olmecSpeakTime = 0
+        self.olmecSays = ""
+    end
 end
 
 function PlayState:draw()
@@ -60,6 +78,30 @@ end
 
 function PlayState:mousereleased(x, y, button)
     -- TODO
+end
+
+-- Stuff Olmec Chan Says
+function PlayState:olmecTalk(subject)
+    -- If Olmec is not currently speaking, sayeth something
+    if self.olmecSpeakTime <= 0 then
+        if subject == "Constants.OLMECSUBJECT_INTRO" then
+            self.olmecSays = Constants.OLMECTALK_INTRO
+        elseif subject == "Constants.OLMECSUBJECT_WORLD" then
+            i = math.random(0, (table.getn(self.olmecWorldLines)))
+            self.olmecSays = self.olmecWorldLines[i]
+        elseif subject == "Constants.OLMECSUBJECT_TEMPLE" then
+            i = math.random(0, (table.getn(self.olmecTempleLines)))
+            self.olmecSays = self.olmecTempleLines[i]
+        elseif subject == "Constants.OLMECSUBJECT_ROVER" then
+            i = math.random(0, (table.getn(self.olmecRoverLines)))
+            self.olmecSays = self.olmecRoverLines[i]
+        elseif subject == "Constants.OLMECSUBJECT_FIGHT" then
+            self.olmecSays = Constants.OLMECTALK_FIGHT
+        elseif subject == "Constants.OLMECSUBJECT_DEFEAT" then
+            self.olmecSays = Constants.OLMECTALK_DEFEAT
+        end
+        olmecSpeakTime = Constants.OLMEC_SPEECH_TIME
+    end
 end
 
 return PlayState

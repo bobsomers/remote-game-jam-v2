36840533e410f7b37a22976255f60305fe0c91e5
Paul Morales <pomorales@gmail.com>
2012-09-29 21:22:14 -0700

    Made rovers work with new assets.

--- entities/spirit.lua
+++ entities/spirit.lua
@@ -2,13 +2,13 @@ local Class = require "hump.class"
 local Vector = require "hump.vector"
 local Constants = require "constants"
 
-local Spirit = Class(function(self, collider, curiosity)
+local Spirit = Class(function(self, collider, curiosity, camera)
     self.collider = collider
     self.curiosity = curiosity
+    self.camera = camera
 
     self.SIZE = Vector(20, 20)
     self.MOVE_SPEED = Constants.HELPER_SPEED
-    self.TURN_SPEED = Constants.HELPER_TURN_SPEED
     self.FRAME_DURATION = Constants.HELPER_FRAME_DURATION
 
     self.shape = self.collider:addRectangle(0, 0, self.SIZE.x, self.SIZE.y)
@@ -16,11 +16,12 @@ local Spirit = Class(function(self, collider, curiosity)
     self.collider:addToGroup("spirit", self.shape)
 
     self.frames = {
-        love.graphics.newImage("assets/curiosity1.png"),
-        love.graphics.newImage("assets/curiosity2.png"),
-        love.graphics.newImage("assets/curiosity3.png")
+        love.graphics.newImage("assets/spirit1.png"),
+        love.graphics.newImage("assets/spirit2.png")
     }
 
+    self.head = love.graphics.newImage("assets/spirithead.png")
+    
     self:reset()
 end)
 
@@ -56,11 +57,25 @@ function Spirit:draw()
     local dx = curiosityPosition.x - position.x
     local dy = curiosityPosition.y - position.y
     local rotation = math.atan2(dy, dx) + math.pi / 2
+    
     love.graphics.draw(self.frames[self.frame + 1],
         position.x, position.y,
         rotation,
-        .6, .6,
+        1, 1,
         self.SIZE.x / 2, self.SIZE.y / 2,
+        0, 0
+    )
+    
+    local mouseX, mouseY = self.camera.camera:worldCoords(love.mouse.getX(), love.mouse.getY())
+    local dx = mouseX - position.x
+    local dy = mouseY - position.y
+    local rotation = math.atan2(dy, dx) + math.pi / 2
+    
+    love.graphics.draw(self.head,
+        position.x, position.y,
+        rotation,
+        1, 1,
+        8, 7,
         0, 0
     )
 end
